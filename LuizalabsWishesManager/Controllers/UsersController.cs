00dde035ff4fb5f07c64579576e7fc4c657d5bb9
Adriano Berto <adriano.berto@outlook.com>
2019-08-02 22:31:01 -0300

    Add project files.

---
+++ LuizalabsWishesManager/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using LuizalabsWishesManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuizalabsWishesManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {   
+        [HttpGet]
+        public IEnumerable<string> Get([FromQuery] int page_size, [FromQuery] int page)
+        {
+            return new string[] { "value1", "value2" };
+
+            //var product = products.FirstOrDefault((p) => p.Id == id);
+            //if (product == null)
+            //{
+            //    return NotFound();
+            //}
+            //return Ok(product);
+        }
+     
+        // POST api/values
+        [HttpPost]
+        public ActionResult<HttpStatusCode> Post([FromBody] NewUser user, [FromServices] User service)
+        {
+            var success = service.Save(user);
+
+            if (success) return HttpStatusCode.InternalServerError;
+
+            return HttpStatusCode.OK;
+        }
+    }
+}
---
+++ LuizalabsWishesManager/Models/Whish.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuizalabsWishesManager.Models
+{
+    public class Whish
+    {
+        public List<Product> Products { get; set; }
+    }
+}
