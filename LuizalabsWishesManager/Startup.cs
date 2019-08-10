3b899b8fa0ef886e32e684d1fcf9d4f77a7b6e8b
Adriano Berto <adriano.berto@outlook.com>
2019-08-10 05:41:51 -0300

    Inclusão do swagger

--- LuizalabsWishesManager/Startup.cs
+++ LuizalabsWishesManager/Startup.cs
@@ -16,11 +16,22 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace LuizalabsWishesManager
 {
+    public class TestFilter : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+        {
+            swaggerDoc.Schemes = new string[] { "https" };
+        }
+    }
+
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -32,6 +43,12 @@ namespace LuizalabsWishesManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "LuizalabsWishesManager", Version = "v1" });
+                c.DocumentFilter<TestFilter>();
+            });
 
             IoC.RegisterMappings(services);
             AutoMapperConfig.RegisterMappings(services);
@@ -49,6 +66,13 @@ namespace LuizalabsWishesManager
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s => {
+                s.RoutePrefix = "help";
+                s.SwaggerEndpoint("../swagger/v1/swagger.json", "LuizalabsWishesManager API");
+                s.InjectStylesheet("../css/swagger.min.css");
+            });
 
             app.UseMvc();
         }
